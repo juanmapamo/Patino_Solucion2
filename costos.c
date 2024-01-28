798bf02b38cc0dfb716a5546cf6d2454db028401
Juan Patino <juan.patino@udla.edu.ec>
2024-01-28 17:14:46 -0500

    Solicitar que al usuario que ingrese los datos

--- costos.c
+++ costos.c
@@ -8,3 +8,19 @@ struct OrdenTrabajo {
 
 struct OrdenTrabajo ordenes_de_trabajo[100];
 int num_ordenes = 0;
+
+int main(int argc, char const *argv[])
+{
+    int cantidad_ordenes;
+
+    // Solicitar al usuario la cantidad de órdenes de trabajo
+    printf("Ingrese la cantidad de ordenes de trabajo: ");
+    scanf("%d", &cantidad_ordenes);
+
+    // Verificar que la cantidad ingresada sea válida
+    if (cantidad_ordenes <= 0 || cantidad_ordenes > 100) {
+        printf("La cantidad de ordenes debe estar entre 1 y 100.\n");
+        return 1; 
+    }
+    return 0;
+}
